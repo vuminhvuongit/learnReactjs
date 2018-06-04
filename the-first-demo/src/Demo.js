b7f135e12a434fb55897693c9a6d83f7c95244eb
vuongvm <vuongvm@aoi-sys.vn>
2018-06-04 14:30:25 +0700

    init project

---
+++ the-first-demo/src/Demo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+  render() {
+    return (
+      <div>
+        <h1 class="text-center">Hello Vương</h1>
+        <p class="text-center">This is the first demo for react</p>
+      </div>
+    );
+  }
+}
+
+export default Demo;
